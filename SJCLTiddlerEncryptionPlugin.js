4ad54609301181e593e9082426fb5334a2ce6eeb
Yakov Litvin <yakov.litvin.publicly@gmail.com>
2025-08-11 13:39:29 +0300

    refactor(plugin): remove unused functions

--- SJCLTiddlerEncryptionPlugin.js
+++ SJCLTiddlerEncryptionPlugin.js
@@ -1,10 +1,10 @@
 /***
 |Name       |SJCLTiddlerEncryptionPlugin|
 |Description|Encrypt/Decrypt tiddlers with a password and SJCL library|
-|Author     |Lyall Pearce, Yanshu Wang|
+|Author     |Lyall Pearce, Yanshu Wang, Yakov Litvin|
 |Source |https://github.com/wangyenshu/SJCLTiddlerEncryptionPlugin/blob/main/SJCLTiddlerEncryptionPlugin.js|
 |License|[[Creative Commons Attribution-Share Alike 3.0 License|http://creativecommons.org/licenses/by-sa/3.0/]]|
-|Version|0.2|
+|Version|0.2.1|
 |~CoreVersion|2.4.0|
 |Overrides|store.getSaver().externalizeTiddler(), store.getTiddler() and store.getTiddlerText()|
 
@@ -662,35 +662,4 @@ PasswordPrompt = {
 };
     
 PasswordPrompt.init();
-
-// supporting functions
-
-function strToLongs(s) {  // convert string to array of longs, each containing 4 chars
-    // note chars must be within ISO-8859-1 (with Unicode code-point < 256) to fit 4/long
-    var l = new Array(Math.ceil(s.length/4));
-    for (var i=0; i<l.length; i++) {
-        // note little-endian encoding - endianness is irrelevant as long as 
-        // it is the same in longsToStr() 
-        l[i] = s.charCodeAt(i*4) + (s.charCodeAt(i*4+1)<<8) + 
-                 (s.charCodeAt(i*4+2)<<16) + (s.charCodeAt(i*4+3)<<24);
-    }
-    return l;  // note running off the end of the string generates nulls since 
-}              // bitwise operators treat NaN as 0
-
-function longsToStr(l) {  // convert array of longs back to string
-    var a = new Array(l.length);
-    for (var i=0; i<l.length; i++) {
-        a[i] = String.fromCharCode(l[i] & 0xFF, l[i]>>>8 & 0xFF, 
-                                     l[i]>>>16 & 0xFF, l[i]>>>24 & 0xFF);
-    }
-    return a.join('');  // use Array.join() rather than repeated string appends for efficiency
-}
-
-function escCtrlCh(str) {  // escape control chars etc which might cause problems with encrypted texts
-    return str.replace(/[\0\t\n\v\f\r\xa0'"!]/g, function(c) { return '!' + c.charCodeAt(0) + '!'; });
-}
-
-function unescCtrlCh(str) {  // unescape potentially problematic nulls and control characters
-    return str.replace(/!\d\d?\d?!/g, function(c) { return String.fromCharCode(c.slice(1,-1)); });
-}
 //}}}
